8cd46ab6d74861fc5ce9768d518b018cbc7476c0
Navarr Barnier <me@navarr.me>
2018-04-13 10:03:16 -0400

    Add aliases to !banned

--- src/Command/Banned.js
+++ src/Command/Banned.js
@@ -2,6 +2,7 @@ const Command = require('../Command');
 
 module.exports = Command.extend({
     commandName: 'banned',
+    commandAliases: ['bans', 'appeal', 'appeals'],
     processMessage: function (message, tokens) {
         message.channel.send("We do not discuss bans in the chatroom. If you are banned, please post a ban appeal — it is the fastest way to get your ban handled! <https://shotbow.net/forum/forums/banappeals/>");
     }
