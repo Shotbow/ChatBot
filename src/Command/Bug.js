1ea8fc3eaf7f8d8daa883905aba9e6794e4a1502
Jamsheed Mistri <jmistri7@gmail.com>
2018-04-21 16:36:08 -0700

    Bug link fix (#9)

--- src/Command/Bug.js
+++ src/Command/Bug.js
@@ -4,6 +4,6 @@ module.exports = Command.extend({
     commandName: 'bug',
     commandAliases: ['bugs'],
     processMessage: function (message, tokens) {
-        message.channel.send("Help keep our games stable by reporting bugs here: <https://shotbow.net/forum/p/bugs/>");
+        message.channel.send("Help keep our games stable by reporting bugs here: <https://bugs.shotbow.net>");
     }
 });
