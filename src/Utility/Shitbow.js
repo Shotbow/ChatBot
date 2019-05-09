beea69ce1f3be6c3f07b38cd87f4f5e7c0883f0e
Navarr Barnier <me@navarr.me>
2019-05-09 10:58:34 -0400

    Update terrible person filter

--- src/Utility/Shitbow.js
+++ src/Utility/Shitbow.js
@@ -4,7 +4,7 @@ module.exports = BotModule.extend({
     initialize: function (dependencyGraph) {
         this._super(dependencyGraph);
         this.discordClient.on('message', message => {
-            if (message.content.replace(/\s/g,'').toLowerCase().match(/([s5ｓ]+(ｈ|h|\|\-\|n)+[ｉi1|]+[ｔt7]+|[ｋk]+[uｕ]+[ｓs5]+[ｏo0]+|クソ|ｸｿ)+(([ｂb8]|\|3)+[ｏo0]+[wｗ]+|弓)+/) !== null) message.delete();
+            if (message.content.replace(/\s/g,'').toLowerCase().match(/([s5ｓ]+(ｈ|h|\|\-\|n)+[ｉi1|]+[ｔt7]+|[ｋk]+[uｕ]+[ｓs5]+[ｏo0]+|クソ|ｸｿ|シット|しっと)+(([ｂb8]|\|3)+[ｏo0]+[wｗ]+|弓|ボウ|ぼう)+/) !== null) message.delete();
         });
     }
 });
