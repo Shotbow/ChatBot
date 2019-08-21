878251470116726529ab5373db3bb3f42e40ed3a
Navarr Barnier <me@navarr.me>
2019-08-21 08:48:14 -0400

    Add MC version to !ip - Add alias !connect

--- src/Command/IP.js
+++ src/Command/IP.js
@@ -2,8 +2,8 @@ const Command = require('../Command');
 
 module.exports = Command.extend({
     commandName: 'ip',
-    commandAliases: ['address', 'server'],
+    commandAliases: ['address', 'server', 'connect'],
     processMessage: function (message, tokens) {
-        return message.channel.send(this.i18n.__mf("Come join me on Shotbow using the IP `play.shotbow.net`!"));
+        return message.channel.send(this.i18n.__mf("Come join me on Shotbow using the address `play.shotbow.net` and version 1.12.2!"));
     }
 });
