a61ebd905a1ca1391a6f5f0e1ae1c435e2e117f2
Jamsheed Mistri <jmistri@berkeley.edu>
2020-03-24 19:00:46 -0700

    Convert tabs to spaces, change let and var to const

--- src/Utility/Shitbow.js
+++ src/Utility/Shitbow.js
@@ -1,21 +1,21 @@
 const BotModule = require('../BotModule');
 
 module.exports = BotModule.extend({
-	config: null,
-	dependencies: {
-		'config': 'config'
-	},
-	initialize: function (dependencyGraph) {
-		this._super(dependencyGraph);
-		this.discordClient.on('message', message => {
-			let trimmed = message.content.replace(/\s/g,'').toLowerCase();
-			for (var i in this.config.swearRegexes) {
-				let regex = new RegExp(this.config.swearRegexes[i]);
-				if (trimmed.match(regex)) {
-					message.delete();
-					return;
-				}
-			}
-		});
-	}
+    config: null,
+    dependencies: {
+        'config': 'config'
+    },
+    initialize: function (dependencyGraph) {
+        this._super(dependencyGraph);
+        this.discordClient.on('message', message => {
+            const trimmed = message.content.replace(/\s/g,'').toLowerCase();
+            for (const i in this.config.swearRegexes) {
+                const regex = new RegExp(this.config.swearRegexes[i]);
+                if (trimmed.match(regex)) {
+                    message.delete();
+                    return;
+                }
+            }
+        });
+    }
 });
