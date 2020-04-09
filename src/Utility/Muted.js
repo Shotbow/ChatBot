79264748bbbc9bfda742f0d2ab80d343c368283d
Jamsheed Mistri <jmistri@berkeley.edu>
2020-04-08 19:26:34 -0700

    Change tabs to spaces

--- src/Utility/Muted.js
+++ src/Utility/Muted.js
@@ -1,18 +1,18 @@
 const BotModule = require('../BotModule');
 
 module.exports = BotModule.extend({
-	config: null,
-	dependencies: {
-		'config': 'config'
-	},
-	initialize: function (dependencyGraph) {
-		this._super(dependencyGraph);
-		this.discordClient.on('message', message => {
-			let member = message.member;
-			let hasRoles = member !== null && typeof member.roles !== 'undefined';
-			if (hasRoles && member.roles.has(message.guild.roles.find("name", "Muted").id)) {
-				message.delete();
-			}
-		});
-	}
+    config: null,
+    dependencies: {
+        'config': 'config'
+    },
+    initialize: function (dependencyGraph) {
+        this._super(dependencyGraph);
+        this.discordClient.on('message', message => {
+            let member = message.member;
+            let hasRoles = member !== null && typeof member.roles !== 'undefined';
+            if (hasRoles && member.roles.has(message.guild.roles.find("name", "Muted").id)) {
+                message.delete();
+            }
+        });
+    }
 });
