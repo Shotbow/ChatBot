cb283e8482240201d3b072f3d62e7dabf389bc82
Jamsheed Mistri <jmistri@berkeley.edu>
2020-04-21 20:04:23 -0700

    Update staff list URL

--- src/Command/Staff.js
+++ src/Command/Staff.js
@@ -3,6 +3,6 @@ const Command = require('../Command');
 module.exports = Command.extend({
     commandName: 'staff',
     processMessage: function (message, tokens) {
-        return message.channel.send(this.i18n.__mf("View the official Shotbow Staff List here: <{url}>", {url:'https://shotbow.net/forum/wiki/shotbow-staff'}));
+        return message.channel.send(this.i18n.__mf("View the official Shotbow Staff List here: <{url}>", {url:'https://wiki.shotbow.net/Shotbow_Staff'}));
     }
 });
