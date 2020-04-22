dc13eb2d4e73c217f48709b04dff0950ccd4f8c2
McJeffr <15939020+McJeffr@users.noreply.github.com>
2020-04-23 00:41:58 +0200

    Added shouldDeleteMessage property. Made whowas command async. Fixed a small bug in Command.js.

--- src/Command/Whowas.js
+++ src/Command/Whowas.js
@@ -2,7 +2,7 @@ const Command = require('../Command');
 
 const uuidRegEx = RegExp('^[0-9a-f]{8}-?[0-9a-f]{4}-?[1-5][0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}$', 'i');
 const mcidRegEx = RegExp('^[a-z\\d_]{3,16}$', 'i');
-const maxDisplayed = 10;
+const maxDisplayed = 25;
 const moreInfoUrl = "https://namemc.com/search?q=";
 const messages = {
     error: "Uh oh, there seems to be an error retrieving data from the Mojang API with that input. Verify your input or try again later!",
@@ -17,6 +17,7 @@ const messages = {
 };
 
 module.exports = Command.extend({
+    shouldDeleteMessage: true,
     commandName: 'whowas',
     https: null,
     moment: null,
@@ -24,103 +25,104 @@ module.exports = Command.extend({
         'https': 'https',
         'moment': 'moment'
     },
-    processMessage: function (message, tokens) {
+    processMessage: async function (message, tokens) {
         let i18n = this.i18n;
 
         if (tokens.length < 2) {
-            message.channel.send(i18n.__mf(messages.incorrectUsage));
-            return;
+            return message.channel.send(i18n.__mf(messages.incorrectUsage));
         }
 
         let identifier = tokens[1];
         if (mcidRegEx.test(identifier)) {
-            this.fetchData(`https://api.mojang.com/users/profiles/minecraft/${identifier}`, (res) => {
-                if (res === null) {
-                    message.channel.send(i18n.__mf(messages.usernameNotFound));
-                    return;
-                }
+            let res = await this.fetchData(`https://api.mojang.com/users/profiles/minecraft/${identifier}`);
+            if (res === undefined) {
+                return message.channel.send(i18n.__mf(messages.usernameNotFound));
+            }
+            if (res === false) {
+                return message.channel.send(i18n.__mf(messages.error));
+            }
 
-                if (res === false) {
-                    message.channel.send(i18n.__mf(messages.error));
-                    return;
-                }
+            identifier = res.id.replace('-', ''); // Remove any dashes from the UUID
 
-                identifier = res.id.replace('-', ''); // Remove any dashes from the UUID
+            res = await this.fetchData(`https://api.mojang.com/user/profiles/${identifier}/names`);
+            if (res === false) {
+                return message.channel.send(i18n.__mf(messages.error));
+            }
 
-                this.fetchData(`https://api.mojang.com/user/profiles/${identifier}/names`, (res) => {
-                    if (res === false) {
-                        message.channel.send(i18n.__mf(messages.error));
-                        return;
-                    }
-
-                    message.channel.send(i18n.__mf(messages.names, { input: tokens[1], names: this.processNames(res), url: moreInfoUrl + identifier }));
-                });
-            });
+            return message.channel.send(i18n.__mf(messages.names, {
+                input: tokens[1],
+                names: this.processNames(res),
+                url: moreInfoUrl + identifier
+            }));
         } else if (uuidRegEx.test(identifier)) {
             identifier = identifier.replace('-', ''); // Remove any dashes from the UUID
 
-            this.fetchData(`https://api.mojang.com/user/profiles/${identifier}/names`, (res) => {
-                if (res === null) {
-                    message.channel.send(i18n.__mf(messages.uuidNotFound));
-                    return;
-                }
+            let res = await this.fetchData(`https://api.mojang.com/user/profiles/${identifier}/names`);
+            if (res === undefined) {
+                return message.channel.send(i18n.__mf(messages.uuidNotFound));
+            }
+            if (res === false) {
+                return message.channel.send(i18n.__mf(messages.error));
+            }
 
-                if (res === false) {
-                    message.channel.send(i18n.__mf(messages.error));
-                    return;
-                }
-
-                message.channel.send(i18n.__mf(messages.names, { input: tokens[1], names: this.processNames(res), url: moreInfoUrl + identifier }));
-            });
+            return message.channel.send(i18n.__mf(messages.names, {
+                input: tokens[1],
+                names: this.processNames(res),
+                url: moreInfoUrl + identifier
+            }));
         } else {
-            message.channel.send(i18n.__mf(messages.invalid));
+            return message.channel.send(i18n.__mf(messages.invalid));
         }
     },
-    processNames: function(names) {
+    processNames: function (names) {
         let nameHistory = [];
         if (names.length > maxDisplayed) {
-            nameHistory.push(this.i18n.__mf(messages.moreNames, { amount: names.length - maxDisplayed }));
+            nameHistory.push(this.i18n.__mf(messages.moreNames, {amount: names.length - maxDisplayed}));
         }
         names
             .splice(names.length - maxDisplayed)
             .map(entry => {
                 if (entry.changedToAt) {
-                    nameHistory.push(this.i18n.__mf(messages.changedName, { name: entry.name, date: this.moment(entry.changedToAt).format('ll') }));
+                    nameHistory.push(this.i18n.__mf(messages.changedName, {
+                        name: entry.name,
+                        date: this.moment(entry.changedToAt).format('ll')
+                    }));
                 } else {
-                    nameHistory.push(this.i18n.__mf(messages.originalName, { name: entry.name }));
+                    nameHistory.push(this.i18n.__mf(messages.originalName, {name: entry.name}));
                 }
-        });
+            });
 
         return nameHistory.join('\n');
     },
-    fetchData: function(url, callback) {
-        try {
-            this.https.get(url, res => {
-                // We need to check if the status is 200, due to Mojang enforcing proper HTTP status codes
-                // In the case of success without content, 204 NO CONTENT is sent, which https accepts as success
-                if (res.statusCode !== 200) {
-                    callback(null);
-                    return;
-                }
-
-                let responseData = '';
-                res.on('data', data => {
-                    responseData += data;
-                });
-                res.on('end', () => {
-                    try {
-                        responseData = JSON.parse(responseData);
-                    } catch (error) {
-                        callback(false);
+    fetchData: function (url) {
+        return new Promise((resolve, reject) => {
+            try {
+                this.https.get(url, res => {
+                    // We need to check if the status is 200, due to Mojang enforcing proper HTTP status codes
+                    // In the case of success without content, 204 NO CONTENT is sent, which https accepts as success
+                    if (res.statusCode !== 200) {
+                        reject(null);
                     }
-                    callback(responseData);
+
+                    let responseData = '';
+                    res.on('data', data => {
+                        responseData += data;
+                    });
+                    res.on('end', () => {
+                        try {
+                            responseData = JSON.parse(responseData);
+                        } catch (error) {
+                            reject(false);
+                        }
+                        resolve(responseData);
+                    });
+                    res.on('error', () => {
+                        reject(false);
+                    });
                 });
-                res.on('error', () => {
-                    callback(false);
-                });
-            });
-        } catch (error) {
-            callback(false);
-        }
+            } catch (error) {
+                reject(false);
+            }
+        });
     }
 });
