8092bea23d6d7b2498ca3d59e601ce6fe60c2e9c
McJeffr <mcjeffr@gmail.com>
2020-07-28 22:20:41 +0200

    Removed !ts command, as we no longer have a TS server.

--- src/Command/TS.js
+++
@@ -1,9 +0,0 @@
-const Command = require('../Command');
-
-module.exports = Command.extend({
-    commandName: 'ts',
-    commandAliases: ['teamspeak', 'mumble'],
-    processMessage: function (message, tokens) {
-        return message.channel.send(this.i18n.__mf("You can connect to our TeamSpeak at `{url}` (but, by personal preference, Discord is better).", {url: 'ts.shotbow.net'}));
-    }
-});
