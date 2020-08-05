f8aa99423b7eff2c3db5e7b83eae45099e97c428
McJeffr <mcjeffr@gmail.com>
2020-08-05 22:56:23 +0200

    Fixed an issue where the implementation of hasRoles is problematic in v12.

--- src/Helper/RoleHelper.js
+++ src/Helper/RoleHelper.js
@@ -2,7 +2,7 @@ const config = require('config');
 
 module.exports = {
     hasRoles: function (member) {
-        return member !== null && typeof member.roles !== 'undefined';
+        return member !== null && member.roles.cache.array().length > 0;
     },
     isMuted: function (member) {
         return this.hasRoles(member) && member.roles.cache.has(config.mutedRole);
