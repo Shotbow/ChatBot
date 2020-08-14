9e0f1a39eefb05e821220f63022d26046eea8c0f
McJeffr <mcjeffr@gmail.com>
2020-08-14 21:20:38 +0200

    Fixed an issue where the support roles couldn't use support commands.

--- src/Helper/RoleDeterminer.js
+++ src/Helper/RoleDeterminer.js
@@ -4,6 +4,9 @@ module.exports = {
     hasRole: function (member, roleName) {
         return member.roles.cache.has(member.guild.roles.cache.find(role => role.name === roleName).id)
     },
+    hasOneOfRoles: function (member, roleIds) {
+        return roleIds.some(roleId => member.roles.cache.has(roleId));
+    },
     isMuted: function (member) {
         return member.roles.cache.has(config.mutedRole);
     },
