e5692f893648cff0d4c9b973daad953e29acde23
McJeffr <mcjeffr@gmail.com>
2020-08-20 01:02:46 +0200

    Added auto-closing functionality. Moved some duplicate code to SupportRoomHelper.js.

--- src/Command/Support.js
+++ src/Command/Support.js
@@ -1,15 +1,12 @@
-const crypto = require('crypto');
 const config = require('config'); // Explicit import is necessary here for command aliases
 
 const Command = require('../Command');
 const RoleDeterminer = require('../Helper/RoleDeterminer');
-const timeoutPromise = require('../Helper/TimeoutPromise');
-const getChannelLog = require('../Helper/ChannelLogger');
+const { generateRoomName, parseRoomType, convertRoom, archiveRoom } = require('../Helper/SupportRoomHelper');
 
 const messages = {
     'help': 'You can use `!support <type> <IGN>` to create a room where you can contact the staff team for support in private, where `<IGN>` is your Minecraft username and `<type>` is one of the following:{types}',
     'type': '\n- `{type}`: {description}',
-    'none': '*none*',
     'unknownType': 'Unknown support type `{type}`.\n\n{help}',
     'roomCreated': 'I\'ve created a support room of type `{type}` and added you to it!',
     'roomConverted': 'I\'ve converted this room\'s support type from `{oldType}` to `{newType}`.',
@@ -21,11 +18,6 @@ const messages = {
     'noIgnProvided': 'It looks like you forgot to add your Minecraft username to the support command. Usage: `!{command} <IGN>`, where `<IGN>` refers to your Minecraft username.',
     'noIgnProvidedWithType': 'It looks like you forgot to add your Minecraft username to the support command. Usage: `!{command} {type} <IGN>`, where `<IGN>` refers to your Minecraft username.',
     'notASupportRoom': 'You can only execute this command in a support room.',
-    'roomConversionTimeout': 'Sorry, I could not rename the channel and thus not convert the support room\'s type. This could be caused by Discord rate limiting me. Try again in a few minutes!',
-    'roomConversionSameType': 'Hey! This support room is already of type `{type}`.',
-    'roomConversionParseError': 'Sorry, I could not parse the support room type from its name. Was it maybe renamed?',
-    'roomConversionNotEnoughArguments': 'You haven\'t provided me with a type to convert to! Use `!support convert <type>` instead.',
-    'roomConversionUnknownType': 'Sorry, I cannot convert this room to support type `{type}` as that type does not exist.',
     'roomCreationError': 'Sorry, it appears that I have encountered an error whilst trying to create a room. Check back later!',
     'supportWelcome': 'Hello {user} (IGN: {IGN})! I have created this room for you to ask a question relating to `{type}` to the staff team in private. Before asking your question, please read through the support rules below.',
     'supportRules': '**• Do not ping staff**\n' +
@@ -71,10 +63,10 @@ module.exports = Command.extend({
             }
 
             if (tokens[0].toLowerCase() === 'close') {
-                return await this.processDeletion(message, commandParameters);
+                return await archiveRoom(message, this.i18n, this.discordClient);
             }
             if (tokens[0].toLowerCase() === 'convert') {
-                return await this.processConversion(message, tokens);
+                return await convertRoom(message, tokens, this.i18n, this.discordClient);
             }
         }
 
@@ -106,7 +98,7 @@ module.exports = Command.extend({
 
         /* Create the channel and add the correct people to it */
         const supportCategory = message.guild.channels.cache.get(this.config.support.category);
-        const supportChannel = await message.guild.channels.create(this.generateRoomName(typeKey), {
+        const supportChannel = await message.guild.channels.create(generateRoomName(typeKey), {
             type: 'text',
             parent: supportCategory,
         })
@@ -138,8 +130,7 @@ module.exports = Command.extend({
             })
             .catch(() => {
                 /* If something went wrong with room creation, we delete it (if it was created) and notify the user */
-                supportChannel.delete().catch(() => {
-                });
+                supportChannel.delete().catch(() => {});
                 return message.channel.send(this.i18n.__mf(messages.roomCreationError));
             });
 
@@ -167,13 +158,13 @@ module.exports = Command.extend({
                 {time: automaticDelete});
             collector.on('end', async (_, reason) => {
                 if (reason === 'time') {
-                    await this.processDeletion(welcomeMessage);
+                    await archiveRoom(welcomeMessage, this.i18n, this.discordClient);
                 }
             });
         }
 
         collector.once('collect', (message) => {
-            const supportType = this.parseRoomType(supportChannel.name);
+            const supportType = parseRoomType(supportChannel.name);
             const supportRoles = this.config.support.types[supportType].roles
                 .map(supportRole => `<@&${supportRole}>`)
                 .join(', ');
@@ -181,122 +172,6 @@ module.exports = Command.extend({
             message.channel.send(this.i18n.__mf(messages.supportMessageReceived, {roles: supportRoles}));
             collector.stop();
         });
-    },
-    processDeletion: async function (message) {
-        await message.channel.send(this.i18n.__mf(messages.roomClosing));
-        const logFile = await getChannelLog(message.channel);
-        await message.channel.delete("Support room closed").catch(() => {
-        });
-
-        /* Notify the moderation channel */
-        const guild = message.channel.guild;
-        const logChannel = guild.channels.cache.get(this.config.moderationLogsRoom);
-        if (!logChannel) {
-            console.log(`Could not post log for deletion of support room \`${message.channel.name}\`: 
-            invalid configuration of log channel`);
-            return;
-        }
-
-        /* Fetch all the participants and turn them into a displayable string */
-        const participants = (await this.getRoomParticipants(message.channel))
-            .filter(participant => participant.id !== this.discordClient.user.id)
-            .map(participant => participant.displayName)
-            .join(', ');
-
-        const embedMessage = await logChannel.send({
-            embed: {
-                color: 0x2196f3,
-                author: {
-                    name: this.discordClient.user.username,
-                    icon_url: this.discordClient.user.displayAvatarURL()
-                },
-                title: this.i18n.__mf(messages.roomClosed, {
-                    user: `@${message.author.username}`,
-                    room: message.channel.name
-                }),
-                fields: [
-                    {
-                        name: this.i18n.__mf(messages.supportType),
-                        value: this.parseRoomType(message.channel.name)
-                    },
-                    {
-                        name: this.i18n.__mf(messages.roomParticipants),
-                        value: participants.length === 0 ? this.i18n.__mf(messages.none) : participants
-                    }
-                ],
-                timestamp: new Date(),
-                footer: {
-                    icon_url: this.discordClient.user.displayAvatarURL(),
-                    text: "Shotbow Chat Bot"
-                }
-            }
-        });
-        const fileMessage = await logChannel.send({files: [logFile]});
-        return [embedMessage, fileMessage];
-    },
-    processConversion: async function (message, tokens) {
-        const supportRoom = message.channel;
-        tokens.shift();
-        if (tokens.length === 0) {
-            return supportRoom.send(this.i18n.__mf(messages.roomConversionNotEnoughArguments));
-        }
-
-        /* Fetch the old type */
-        const oldTypeKey = this.parseRoomType(supportRoom.name);
-        const oldType = this.config.support.types[oldTypeKey];
-        if (!oldTypeKey) {
-            return supportRoom.send(this.i18n.__mf(messages.roomConversionParseError));
-        }
-
-        /* Fetch the new type */
-        const newTypeKey = tokens[0];
-        const newType = this.config.support.types[newTypeKey];
-        if (!newType) {
-            return supportRoom.send(this.i18n.__mf(messages.roomConversionUnknownType, {type: newTypeKey}));
-        }
-
-        /* If we are switching to the same type, do nothing */
-        if (oldTypeKey === newTypeKey) {
-            return supportRoom.send(this.i18n.__mf(messages.roomConversionSameType, {type: newTypeKey}));
-        }
-
-        /* Rename the channel. Discord's Rate Limiting policies are really strict on this one, so we can timeout */
-        let continueConversion = true;
-        await timeoutPromise(supportRoom.setName(this.generateRoomName(newTypeKey)), 5000).catch(() => {
-            continueConversion = false;
-            return supportRoom.send(this.i18n.__mf(messages.roomConversionTimeout));
-        });
-        if (!continueConversion) {
-            return;
-        }
-
-        /* Remove old support roles and add new ones */
-        for (const supportRoleId of oldType.roles) {
-            await supportRoom.permissionOverwrites.get(supportRoleId)
-                .delete('Support room type conversion')
-                .catch((e) => {
-                    console.log(e)
-                });
-        }
-        for (const supportRoleId of newType.roles) {
-            await supportRoom.updateOverwrite(supportRoleId, {
-                ADD_REACTIONS: true,
-                ATTACH_FILES: true,
-                EMBED_LINKS: true,
-                READ_MESSAGE_HISTORY: true,
-                SEND_MESSAGES: true,
-                USE_EXTERNAL_EMOJIS: true,
-                SEND_TTS_MESSAGES: true,
-                VIEW_CHANNEL: true
-            }, 'Support room type conversion').catch((e) => {
-                console.log(e)
-            });
-        }
-
-        return supportRoom.send(this.i18n.__mf(messages.roomConverted, {
-            oldType: oldTypeKey,
-            newType: newTypeKey
-        }));
     },
     extractParameters: function (tokens) {
         const clonedTokens = [...tokens];
@@ -331,36 +206,5 @@ module.exports = Command.extend({
             supportType,
             ign
         }
-    },
-    generateRoomName: function (typeKey) {
-        return `support-${typeKey}-${crypto.randomBytes(4).toString('hex')}`
-    },
-    parseRoomType: function (roomName) {
-        /* Split on the dashes in the room name */
-        const splitRoomName = roomName.split("-");
-        if (splitRoomName.length < 2) {
-            return null;
-        }
-
-        /* Fetch the type from the second entry in the split array and validate it */
-        const typeKey = splitRoomName[1];
-        if (!this.config.support.types[typeKey]) {
-            return null;
-        }
-
-        return typeKey;
-    },
-    getRoomParticipants: async function (channel) {
-        const guild = channel.guild;
-        const permissionOverwrites = channel.permissionOverwrites.array();
-        const participants = [];
-        for (const permissionOverwrite of permissionOverwrites) {
-            await guild.members.fetch(permissionOverwrite.id)
-                .then(participant => participants.push(participant))
-                .catch(() => {
-                });
-        }
-
-        return participants;
     }
 });
---
+++ src/Utility/SupportCloser.js
@@ -0,0 +1,74 @@
+const BotModule = require('../BotModule');
+const { archiveRoom } = require('../Helper/SupportRoomHelper');
+
+const messages = {
+    'warning': 'It appears that there was no activity in this room for a while. If no more activity takes place, I will automatically close this channel.'
+}
+const interval = 60 * 60 * 1000;
+
+module.exports = BotModule.extend({
+    config: null,
+    i18n: null,
+    moment: null,
+    dependencies: {
+        'config': 'config',
+        'i18n': 'i18n',
+        'moment': 'moment'
+    },
+    initialize: function (dependencyGraph) {
+        this._super(dependencyGraph);
+
+        this.discordClient.once("ready", () => {
+            const supportCategory = this.retrieveSupportCategory();
+            if (!supportCategory || !supportCategory.children) {
+                console.log("WARNING: could not retrieve support category for automatic support closer utility");
+                return;
+            }
+
+            setInterval(() => {
+                const supportRooms = supportCategory.children;
+                supportRooms.forEach(async supportRoom => {
+                    const lastMessage = await this.retrieveLastMessage(supportRoom);
+                    if (!lastMessage) {
+                        return;
+                    }
+
+                    const now = this.moment();
+
+                    /* Check if we should send a warning notice */
+                    let messageTimestamp = this.moment(lastMessage.createdTimestamp);
+                    if (now.isAfter(messageTimestamp.add(this.config.support.autoClose.warning, 'ms'))
+                        && lastMessage.content !== messages.warning) {
+                        supportRoom.send(this.i18n.__mf(messages.warning, {timeInactivity: 0, timeToClose: 0}));
+                        return;
+                    }
+
+                    /* Check if we should close the channel (moment mutates the timestamp, hence the re-init) */
+                    messageTimestamp = this.moment(lastMessage.createdTimestamp);
+                    if (lastMessage.content === messages.warning
+                        && now.isAfter(messageTimestamp.add(this.config.support.autoClose.closing, 'ms'))) {
+                        await archiveRoom(lastMessage, this.i18n, this.discordClient);
+                    }
+                });
+            }, interval);
+        });
+    },
+    retrieveSupportCategory: function () {
+        /* We don't keep track of the guild ID, but we can find it by looking for the support channel in all guilds */
+        let supportCategory = null;
+        this.discordClient.guilds.cache.forEach(guild => {
+            const matchedChannel = guild.channels.cache.get(this.config.support.category);
+            if (matchedChannel) {
+                supportCategory = matchedChannel;
+            }
+        });
+        return supportCategory;
+    },
+    retrieveLastMessage: async function (channel) {
+        const lastMessage = await channel.messages.fetch({limit: 1});
+        if (!lastMessage || !lastMessage.first()) {
+            return null;
+        }
+        return lastMessage.first();
+    }
+});
