e27a044c6610f125bfbfc100c54cc5a413da9b13
Navarr Barnier <me@navarr.me>
2020-08-29 13:51:56 -0400

    Update text and documentatio blockn

--- src/Utility/NitroBoost.js
+++ src/Utility/NitroBoost.js
@@ -1,12 +1,14 @@
 const BotModule = require('../BotModule');
 
 const Message = {
-    boost: '<@{discordId}> Thanks for boosting!\\nIf you would like to claim your in-game rank, please [private message {upgrader} on the forums]({link}) with your Discord Username and Discriminator ({username}#{discriminator})'
+    boost: "<@{discordId}> Thanks for boosting!\nIf you would like to claim your in-game rank, please private message {upgrader} on the forums with the title \"Nitro role\" as well as your Discord Username and Discriminator ({username}#{discriminator})\n<{link}>"
 }
 
 module.exports = BotModule.extend({
+    config: null,
     i18n: null,
     dependencies: {
+        'config': 'config',
         'i18n': 'i18n'
     },
 
@@ -16,6 +18,10 @@ module.exports = BotModule.extend({
         this.discordClient.on('guildMemberUpdate', this.checkNewBoost.bind(this));
     },
 
+    /**
+     * @param {GuildMember} oldMember
+     * @param {GuildMember} newMember
+     */
     checkNewBoost: function (oldMember, newMember) {
         if (oldMember.premiumSinceTimestamp !== null || newMember.premiumSinceTimestamp === null) {
             // If they were already premium, or they are not premium now - they didn't just get premium.
