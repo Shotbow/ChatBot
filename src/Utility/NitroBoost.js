f1e76f90652c643b1d703a05f668a3cf5bf25a3a
Navarr Barnier <me@navarr.me>
2020-09-26 15:16:12 -0400

    Correct bug in booster thanking

--- src/Utility/NitroBoost.js
+++ src/Utility/NitroBoost.js
@@ -30,7 +30,7 @@ module.exports = BotModule.extend({
             return;
         }
 
-        newMember.guild.channels.find(this.config.nitro.channel).send(
+        newMember.guild.channels.cache.find(this.config.nitro.channel).send(
             this.i18n.__mf(
                 messages.boost,
                 {
