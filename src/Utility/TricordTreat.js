45e4f303c68e71d21ff152263f6ac59971a3a793
Navarr Barnier <me@navarr.me>
2020-10-17 23:03:39 -0400

    Add code to automatically delete Trick'cord Treat messages

---
+++ src/Utility/TricordTreat.js
@@ -0,0 +1,64 @@
+const BotModule = require('../BotModule');
+
+const ONE_MINUTE_IN_MS = 60 * 1000;
+
+module.exports = BotModule.extend({
+    config: null,
+    timeout: null,
+    dependencies: {
+        'config': 'config'
+    },
+    trickcordTreatCommands :[
+        'h!trick',
+        'h!tricks',
+        'h!treat',
+        'h!treats',
+        'h!leaderboard',
+        'h!help',
+        'h!support',
+        'h!invite',
+        'h!inventory'
+    ],
+
+    initialize: function (dependencyGraph) {
+        this._super(dependencyGraph);
+
+        this.discordClient.on('message', this.handleMessage.bind(this));
+
+        this.timeout = typeof this.config.trickcordTreatDeleteTime != 'undefined' ?
+            this.config.trickcordTreatDeleteTime :
+            ONE_MINUTE_IN_MS;
+
+        console.log(this.config.trickcordTreatRoom);
+    },
+
+    /**
+     *
+     * @param {Message} message
+     */
+    handleMessage: function (message) {
+        if (!message.guild) {
+            return;
+        }
+        if (!message.member) {
+            return;
+        }
+
+        if (typeof this.config.trickcordTreatRoom !== 'undefined'
+            && message.channel.id == this.config.trickcordTreatRoom) {
+            return;
+        }
+
+        message.member.is
+
+        const isTricordTreatCommand = this.trickcordTreatCommands.indexOf(message.content) >= 0;
+        const isTricordTreatBot = message.member.id == '755580145078632508';
+        if (!isTricordTreatBot && !isTricordTreatCommand) {
+            return; // Not a message we care about
+        }
+
+        setTimeout(() => {
+            message.delete().catch(() => {});
+        }, this.timeout);
+    }
+});
