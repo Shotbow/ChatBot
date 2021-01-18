3c5c1bf7cb490ef6b5da5cee6d3de39f5c5f8d6f
Jamsheed Mistri <jmistri@berkeley.edu>
2021-01-17 19:17:36 -0800

    Remove stuck command - there is now a support command which handles stuck-in-a-block issues, and the old thread is now locked

--- src/Command/Stuck.js
+++
@@ -1,8 +0,0 @@
-const Command = require('../Command');
-
-module.exports = Command.extend({
-    commandName: 'stuck',
-    processMessage: function (message, tokens) {
-        return message.channel.send(this.i18n.__mf("Stuck in a block or area on MineZ? Request to be moved here: <https://shotbow.net/forum/threads/stuck-in-a-block-or-area.266338/>"));
-    }
-});
