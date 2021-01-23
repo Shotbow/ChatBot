5420ee97066f44542d2cd0fed55985bb13733425
Jamsheed Mistri <jmistri@berkeley.edu>
2021-01-22 17:38:17 -0800

    Update ban message in English, Japanese, and Spanish, to give a hint about reddit

--- src/Command/Banned.js
+++ src/Command/Banned.js
@@ -4,6 +4,6 @@ module.exports = Command.extend({
     commandName: 'banned',
     commandAliases: ['bans', 'appeal', 'appeals'],
     processMessage: function (message, tokens) {
-        return message.channel.send(this.i18n.__mf("We do not discuss bans in the chatroom. If you are banned, please post a ban appeal — it is the fastest way to get your ban handled! <https://shotbow.net/forum/forums/banappeals/>"));
+        return message.channel.send(this.i18n.__mf("We do not discuss bans in the chatroom. If you are banned, please post a ban appeal — it is the fastest way to get your ban handled! <https://shotbow.net/forum/forums/banappeals/>\n\nIf you do not have an account registered on the forums, this thread will also tell you an alternative way to appeal. Please read the entire thread thoroughly."));
     }
 });
