e4b21fb8bdc86f66e63cf174792c854f0994685b
Jamsheed Mistri <jmistri@berkeley.edu>
2021-04-11 14:34:10 -0700

    Fix bug message

--- src/Command/Bug.js
+++ src/Command/Bug.js
@@ -4,6 +4,6 @@ module.exports = Command.extend({
     commandName: 'bug',
     commandAliases: ['bugs'],
     processMessage: function (message, tokens) {
-        return message.channel.send(this.i18n.__mf("Help keep our games stable by reporting bugs here: <{url}>", {url: 'https://bugs.shotbow.net'}));
+        return message.channel.send(this.i18n.__mf("Help keep our games stable by reporting bugs! You can report a bug by executing the command `!support bug <IGN>`."));
     }
 });
