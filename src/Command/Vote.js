1e2744f092b200a2c0a70a21b5eb4a0ae5b32862
Swipe <70344296+jeremiah-kim@users.noreply.github.com>
2022-02-02 07:09:49 -0500

    Update Vote.js

--- src/Command/Vote.js
+++ src/Command/Vote.js
@@ -3,6 +3,6 @@ const Command = require('../Command');
 module.exports = Command.extend({
     commandName: 'vote',
     processMessage: function (message, tokens) {
-        return message.channel.send(this.i18n.__mf('Support Shotbow for free by using our vote dashboard: https://vote.shotbow.net/')));
+        return message.channel.send(this.i18n.__mf('Support Shotbow for free by using our vote dashboard: <https://vote.shotbow.net/>')));
     }
 });
