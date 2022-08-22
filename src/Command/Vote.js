8d946134364e4a1b6f0c7d3740bcdb19360ec8ba
Jamsheed Mistri <jmistri@berkeley.edu>
2022-08-21 21:34:01 -0700

    Update voting text

--- src/Command/Vote.js
+++ src/Command/Vote.js
@@ -3,6 +3,6 @@ const Command = require('../Command');
 module.exports = Command.extend({
     commandName: 'vote',
     processMessage: function (message, tokens) {
-		return message.channel.send(this.i18n.__mf('Support us for free by voting for Shotbow! Learn more on our Vote Dashboard at <https://vote.shotbow.net>'));
+		return message.channel.send(this.i18n.__mf('Vote for Shotbow on our Vote Dashboard: <https://vote.shotbow.net>\n\nYou will receive a 100 XP reward for each vote you cast. Votes will additionally contribute to the community counter for even more free XP!'));
     }
 });
