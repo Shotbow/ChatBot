ce639a1c0c8ef2fd1f56e4a21f5de4424d2c5c95
Jamsheed Mistri <jmistri@berkeley.edu>
2023-09-13 21:43:11 -0700

    Remove vote command

--- src/Command/Vote.js
+++
@@ -1,8 +0,0 @@
-const Command = require('../Command');
-
-module.exports = Command.extend({
-    commandName: 'vote',
-    processMessage: function (message, tokens) {
-		return message.channel.send(this.i18n.__mf('Vote for Shotbow on our Vote Dashboard: <https://vote.shotbow.net>\n\nYou will receive a 100 XP reward for each vote you cast. Votes will additionally contribute to the community counter for even more free XP!'));
-    }
-});
